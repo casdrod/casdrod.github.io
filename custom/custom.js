4468a1c287d19e799a00f60acc1e264294273858
Casey Rodriguez <casdrod@gmail.com>
2020-06-27 20:12:00 -0500

    opacity for scroll elements changed to CSS instead of JS to not show on load

--- custom/custom.js
+++ custom/custom.js
@@ -1,22 +1,17 @@
 const personal = document.querySelector('#personal');
 const pro = document.querySelector('#pro');
 const aboutTitle = document.querySelector('.about-title');
-const aboutText = document.querySelector('.about-text');
+let aboutText = document.querySelector('.about-text');
 const arrow = document.querySelector('.down-arrow');
 const hiddenElements = document.querySelectorAll('.hidden');
 const workCards = document.querySelectorAll('.work-card');
 const workTitle = document.querySelector('.work-title');
 const workText = document.querySelector('.work-text');
 
-window.addEventListener('load', () => {
-    hiddenElements.forEach(hiddenEl => {
-        hiddenEl.classList.add('no-opacity');
-    });
-});
-
 
 window.addEventListener('scroll', () => {
     var scroll = window.scrollY;
+    console.log(scroll);
     if (scroll >= 180) {
         aboutTitle.classList.add('fade-in');
     } if (scroll >= 180 && window.innerWidth <= 899) {
@@ -28,17 +23,17 @@ window.addEventListener('scroll', () => {
     } if (scroll >= 450 && window.innerWidth >= 900) {
         personal.classList.add("personal-animation");
         pro.classList.add("pro-animation");
-    } if (scroll >= 550) {
+    } if (scroll >= 600) {
         workTitle.classList.add('fade-in');
         workText.classList.add('fade-in');
     }
 });
 
 
-/*Observer for Work Cards*/
+/*Observers*/
 let options = {
     root: null,
-    threshold: 0.5
+    threshold: 0.6
 };
 
 var observer = new IntersectionObserver(entries => {
@@ -48,6 +43,7 @@ var observer = new IntersectionObserver(entries => {
         }
     });
 }, options);
+
 
 workCards.forEach(card => {observer.observe(card)});
 
