6c35c79b956c219377123a7c73c04a89010b8639
Casey Rodriguez <casdrod@gmail.com>
2020-07-21 18:43:18 -0500

    Intersection Observer did not work 100% of the time so replaced with better option

--- custom/custom.js
+++ custom/custom.js
@@ -1,41 +1,50 @@
-const personal = document.querySelector('#personal');
-const pro = document.querySelector('#pro');
+const arrow = document.querySelector('.down-arrow');
 const aboutTitle = document.querySelector('.about-title');
 let aboutText = document.querySelector('.about-text');
-const arrow = document.querySelector('.down-arrow');
-const workTitle = document.querySelector('.work-title');
-const workText = document.querySelector('.work-text');
-const workCards = document.querySelectorAll('.work-card');
-
+const personal = document.querySelector('#personal');
+const pro = document.querySelector('#pro');
 
 /*Scroll event for about section and arrow*/
 window.addEventListener('scroll', () => {
     var scroll = window.scrollY;
     if (scroll >= 180) {
         aboutTitle.classList.add('fade-in');
-    } if (scroll >= 180 && window.innerWidth <= 899) {
+        aboutText.classList.add('fade-in');
+    } if (scroll >= 280 && window.innerWidth <= 899) {
         personal.classList.add('fade-in');
         pro.classList.add('fade-in');
     } if (scroll >= 475) {
         arrow.classList.add('no-opacity');
-        aboutText.classList.add('fade-in');
-    } if (scroll >= 450 && window.innerWidth >= 900) {
+    } if (scroll >= 400 && window.innerWidth >= 900) {
         personal.classList.add("personal-animation");
         pro.classList.add("pro-animation");
     }
 });
 
+/*Work section function*/
+(function() {
+  var elements;
+  var windowHeight;
 
-/*Observer for work section*/
-var observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.intersectionRatio > 0) {
-            entry.target.classList.add('fade-in');
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    elements.forEach(element => {
+        var positionFromTop = element.getBoundingClientRect().top;
+
+        if (positionFromTop - windowHeight <= 0) {
+            element.classList.add('fade-in');
+            element.classList.remove('hidden');
         }
     });
-});
+  }
 
-observer.observe(workTitle);
-observer.observe(workText);
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
 
-workCards.forEach(card => {observer.observe(card)});
+  init();
+  checkPosition();
+})();
